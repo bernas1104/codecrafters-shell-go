e006431a80da0c7dc4ebdc11ec74c1c875930f7e
bernas1104 <bernardoc1104@gmail.com>
2024-12-06 15:57:14 -0300

    handling invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,9 +9,23 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+const COMMAND_NOT_FOUND = "command not found"
+
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	read, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		panic("Error!")
+	}
+
+	command := read[0:getReadSize(read)]
+
+	fmt.Printf("%v: %v\n", command, COMMAND_NOT_FOUND)
+}
+
+func getReadSize(read string) int {
+	return len(read) - 1
 }
